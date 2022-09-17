6b03c061f8df313b0839e4f175ead9386dfbb2e0
EsmeraldaEsp <93735201+EsmeraldaEsp@users.noreply.github.com>
2022-09-16 20:44:42 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,95 @@
+///se muestra rapidamente el codigo de la app del clima 
+
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default class WeatherScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weather: '',
+    };
+  }
+
+  getWeather = async () => {
+    //cambiar latitud y longitud 
+    ///lat=
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lat=23&lon=102';
+    return fetch(url)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          weather: responseJson,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  componentDidMount = () => {
+    this.getWeather();
+  };
+
+  render() {
+    if (this.state.weather === '') {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.subContainer}>
+            <Text style={styles.title}>
+              Pronostico del Clima
+            </Text>
+            <Image
+              style={styles.cloudImage}
+              source={require('./nube.jpg')}
+            />
+            <View style={styles.textContainer}>
+            <Text style={{ fontSize: 18}}>
+              {this.state.weather.main.temp}&deg;C
+            </Text>
+            <Text style={{ fontSize: 20, margin:10}}>
+              humedad : {this.state.weather.main.humidity}
+            </Text>
+            <Text style={{fontSize: 20}}>
+              {this.state.weather.weather[0].description}
+            </Text>
+          </View>
+          </View>    
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex:1
+  },
+  subContainer : { 
+    flex: 1, 
+    borderWidth: 1, 
+    alignItems: 'center' 
+    },
+    title:{ 
+      marginTop: 50, 
+      fontSize: 30,
+      fontWeight: '550' 
+    },
+    cloudImage :{ 
+      width: 200, 
+      height: 200, 
+      marginTop: 30 
+    },
+    textContainer : { 
+      flex: 1,
+      alignItems: 'center', 
+      flexDirection:'row', 
+      marginTop:-150
+    }
+});
